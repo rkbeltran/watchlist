cb0f06fac2923c3c4c804382f9be3cd54bd0100a
Rachel Beltran <rachelbeltran@Rachels-MacBook-Pro-3.local>
2025-10-21 10:25:18 -0400

    Resolved merge conflicts

--- pages/Survey.py
+++ pages/Survey.py
@@ -96,8 +96,4 @@ else:
     st.warning("Your watchlist is empty :) Add shows to get started!")
 
 st.divider()
-<<<<<<< HEAD
 st.image("Images/second-years.jpeg")
-=======
-st.image("Images/second-years.jpeg")
->>>>>>> badc5621c6373821ef9cd32f90820eb9de6e9698
